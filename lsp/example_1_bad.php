f5fcc27c4ecd539d1b6eb5ea711c62d9276ff77f
Erick <erickcfuga@gmail.com>
2019-12-13 11:38:29 -0300

    Add Liskov substitution examples

---
+++ lsp/example_1_bad.php
@@ -0,0 +1,49 @@
+<?php
+
+class Signup {
+    public function __construct($attributes) {
+	$this->attributes= $attributes;
+    }
+
+    public function save() {
+	$account = Account::create($this->attributes["account"]);	
+	return $account->users->create($this->atributes);	
+    }
+}
+
+// Subclass violates LSP by changing the save method.
+class InvitationSignup extends Signup {
+    public function save($invitation) {
+	$user = parent::save();
+	$invitation->accept($user);
+	return $user;
+    }
+}
+
+// Callers must check before calling the save method to decide if a parameter
+// should be passed.
+class SignupController extends BaseController {
+    public function store(Request $request) {
+	$signup = $this->buildSignup($request);
+
+       	if ($request->has("invitation_id")) {
+	    $invitation = Invitation::find($request->invitation_id);
+	    $signup->save($invitation);
+
+	    return view('signup');
+	} 
+
+	$signup->save();
+
+	return view('signup');
+    } 
+
+    private function buildSignup(Request $request) {
+	if($request->has("invitation_id")){
+	    return new InvitationSignup($request->getContent());
+	}
+
+	return new Signup($request->getContent());
+    }
+}
+
---
+++ lsp/example_1_good.php
@@ -0,0 +1,35 @@
+<?php
+
+// Subclass follows LSP. The invitation depencency is now injected in the constructor.
+class InvitationSignup extends Signup {
+    public function __construct($attributes, Invitation $invitation) {
+	parent::__construct($attributes);
+	$this->invitation = $invitation;	
+    }
+
+    public function save() {
+	$user = parent::save();
+	$this->invitation->accept($user);
+
+	return $user;
+    }
+}
+
+// Callers must decide only which class to create, not how to call it's methods.
+class SignupController extends BaseController {
+    public function store(Request $request) {
+	$this->buildSignup($request)->save; 
+
+	return view('signup');
+    }
+
+    private function buildSignup(Request $request) {
+	if($request->has("invitation_id")){
+	    $invitation = Invitation::find($request->invitation_id);
+
+	    return new InvitationSignup($request->getContent(), $invitation);
+	}
+
+	return new Signup($request->getContent());
+    }
+}
